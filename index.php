f35ced66f3ff5dd43d1ffe39eb07967feaed2fb6
damarismorellato <dls.morellato19@gmail.com>
2020-01-29 15:57:52 -0300

    css e login

--- index.php
+++ index.php
@@ -5,20 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>DM | Área Restrita</title>
 </head>
 <body>
-    <div class="login-box-body">
-        <div class="login-logo"><img src="logo-login.png" alt="user image" class="img-circle" style="max-width: 30%; "></div>
-    </div>
-
-
-    
-            
-            
-            </table>
+<div class="container text-center">       
+    <div class="row">
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4">
+        <div class="card text-center">
+        <div class="card-body">
+            <h5 class="card-title">DM Tecnologia</h5>
+            <div class="login-logo"><img src="logo-login.png" alt="user image" class="img-circle" style="max-width: 30%;"></div>
+            <form>
+                <div class="form-group">
+                    <label for="Email1">E-mail</label>
+                    <input type="email" class="form-control" id="Email1">
+                </div>
+                <div class="form-group">
+                    <label for="Password1">Senha</label>
+                    <input type="password" class="form-control" id="Password1">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="Check1">
+                    <label class="form-check-label" for="Check1">Lembrar Me</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Entrar</button>
+            </form>
+        </div>
         </div>
     </div>
+    <div class="col-sm-4"></div>
+</div>
+
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
 </body>
 </html>
